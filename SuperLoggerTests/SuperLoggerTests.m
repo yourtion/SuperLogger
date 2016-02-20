7284585ba6153c2449996e011c01563ef5faaee6
YourtionGuo <yourtion@gmail.com>
2016-02-21 00:57:36 +0800

    add Framework

---
+++ SuperLoggerTests/SuperLoggerTests.m
@@ -0,0 +1,39 @@
+//
+//  SuperLoggerTests.m
+//  SuperLoggerTests
+//
+//  Created by YourtionGuo on 2/19/16.
+//  Copyright © 2016 Yourtion. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SuperLoggerTests : XCTestCase
+
+@end
+
+@implementation SuperLoggerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
